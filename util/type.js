32a98aa963ae306f81b8c109361484681c668f76
heisebingxue1 <heisebingxue1@sina.com>
2013-03-28 16:40:05 +0800

    Create type.js

---
+++ util/type.js
@@ -0,0 +1,25 @@
+/*
+    'type' method:: return a string that identifies the data type of an expression.
+*/
+
+(function(global){
+  var i, len, item,
+      classType = {},
+		  arr = ['Boolean', 'Number', 'String', 'Function', 'Array', 'Date', 'RegExp', 'Object'],
+      toString = Object.prototype.toString;
+
+	for(i = 0, len = arr.length; i < len; i++){
+		item = arr[i];
+		classType['[object ' + item + ']'] = item.toLowerCase();
+	}
+
+  function type(obj){
+    return obj == null?
+      String(obj) : 
+      classType[toString.call(obj)] || 'object';
+  }
+
+
+  global.JOY || (global.JOY = {});
+  global.JOY.type = type;
+})(window);
